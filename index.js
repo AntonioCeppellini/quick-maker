cdd6e473cfdac5aa52273f9e008ceae33aabf2b3
AntonioCeppellini <antonio.ceppellini@gmail.com>
2023-09-11 15:52:14 +0200

    finish with comments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
+//import everything that we need
 const inquirer = require('inquirer');
-const { exec } = require('child_process');
-const { spawn } = require('child_process')
-
+const { spawn } = require('child_process');
+//creating the questions that we need in the CLI app using inquirer
 const questions = [
     {
         type: "list",
@@ -15,13 +15,18 @@ const questions = [
         name: "name"
     }
 ];
+//creating the interations that we need in the CLI app using inquirer
 inquirer.prompt(questions).then(answers => {
     console.log(`Creating your ${answers.Project}...`)
+    //initialize the variables that we'll use for the decisions making
     var project_type = answers.Project;
     var project_name = answers.name;
+    //creating our tree of choices 
     if (project_type === "React Project"){
+        //creating the project and initialize it with git init, in angular and vue it will be done without command
         const commandAngular = spawn(`npx create-react-app ${project_name} && cd ${project_name} && git init`, {shell : true, stdio: 'inherit', })
         commandAngular.on('close', (code) => {
+            //code 0 means that everything is going all right
             if (code === 0) {
                 console.log(`Your ${project_type} has been created successfully!`)
             } else {
