16aa04015e29daf3fc83310b2c1add790f755c9b
AntonioCeppellini <128388022+AntonioCeppellini@users.noreply.github.com>
2023-09-12 14:32:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
-import chalk from 'chalk'
-import inquirer from 'inquirer'
-import {spawn} from 'child_process'
+import figlet from 'figlet'; 
+import chalk from 'chalk'; //Imported figlet and chalk to add some style 
+import inquirer from 'inquirer'; //Inquirer allows us to make some interactive cool cli app
+import {spawn} from 'child_process'; //Launching commands from our app to the terminal
 //creating the questions that we need in the CLI app using inquirer
 const questions = [
     {
-        type: "list",
+        type: "list", //List of choices
         name: "Project",
-        choices: ["React Project", "Angular Project", "Vue Project"]
+        message: "What kind of project do you want to start?", //Message that will be displayed into the cli app
+        choices: ["React Project", "Angular Project", "Vue Project"] //Choices that will be taken as value for our variable "project_type"
     },
     {
-        type: "input",
+        type: "input", //The user will input a value that will be taken as value for our variable "project_name"
         message: "What is your project name?",
         name: "name"
     }
 ];
+//Here i have used chalk combined with figlet
+console.log(chalk.blueBright(
+    figlet.textSync("WELCOME", {
+        font: "Rectangles"
+    })
+))
 //creating the interations that we need in the CLI app using inquirer
 inquirer.prompt(questions).then(answers => {
     console.log(chalk.blue(`Creating your ${answers.Project}...`))
@@ -28,6 +36,11 @@ inquirer.prompt(questions).then(answers => {
         commandAngular.on('close', (code) => {
             //code 0 means that everything is going all right
             if (code === 0) {
+                console.log(chalk.greenBright(
+                    figlet.textSync("DONE!", {
+                    font: "Rectangles"
+                    })
+                ))
                 console.log(chalk.green(`Your ${project_type} has been created successfully!`))
             } else {
                 console.log(error)
@@ -38,6 +51,11 @@ inquirer.prompt(questions).then(answers => {
         const commandAngular = spawn(`ng new ${project_name}`, {shell : true, stdio: 'inherit', })
         commandAngular.on('close', (code) => {
             if (code === 0) {
+                console.log(chalk.greenBright(
+                    figlet.textSync("DONE!", {
+                    font: "Rectangles"
+                    })
+                ))
                 console.log(chalk.green(`Your ${project_type} has been created successfully!`))
             } else {
                 console.log(error)
@@ -48,6 +66,11 @@ inquirer.prompt(questions).then(answers => {
         const commandAngular = spawn(`vue create -d ${project_name}`, {shell : true, stdio: 'inherit', })
         commandAngular.on('close', (code) => {
             if (code === 0) {
+                console.log(chalk.greenBright(
+                    figlet.textSync("DONE!", {
+                    font: "Rectangles"
+                    })
+                ))
                 console.log(chalk.green(`Your ${project_type} has been created successfully!`))
             } else {
                 console.log(error)
@@ -55,3 +78,4 @@ inquirer.prompt(questions).then(answers => {
         })
     }
 })
+    
